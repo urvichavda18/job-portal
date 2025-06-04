2d9646ac1ffaf69a7de18a34afc014ca665c2a22
urvichavda188 <urvichavda188@gmail.com>
2025-06-04 17:39:48 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Navbar from './components/shared/Navbar';
-import Login from './components/auth/login';
+// import Navbar from './components/shared/Navbar';
+import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import { RouterProvider } from 'react-router-dom';
 import Home from './components/Home';
 import Jobs from './components/Jobs';
 import Browse from './components/Browse';
+
 import Profile from './components/Profile';
 import JobDescription from './components/JobDescription';
 import Companies from './components/admin/Companies';
